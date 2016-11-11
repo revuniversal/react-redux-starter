43c45e4b65e91ef8d83ce0eb4db6d296b610158a
Rev Herr <revuniversal@gmail.com>
2016-11-11 09:20:57 -0600

    initial commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Welcome to React</h1>
+        
+        <p className="App-intro">
+          To get started, edit src/App.js and save to reload.
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
